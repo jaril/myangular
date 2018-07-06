e188fb9ff22b68baa375eebd65c925bca8eca4af
Dover <jarilvalenciano@gmail.com>
2018-07-06 09:40:56 -0400

    Scope Objects + Watching Object Properties

---
+++ src/scope.js
@@ -0,0 +1,25 @@
+
+// Scope constructor function which is just a regular Object constructor
+
+var _ = require('lodash');
+
+function Scope() {
+  this.$$watchers = [];
+};
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+
+  this.$$watchers.push(watcher)
+};
+
+Scope.prototype.$digest = function() {
+  _.forEach(this.$$watchers, function(watcher) {
+    watcher.listenerFn();
+  })
+}
+
+module.exports = Scope
--- test/hello_spec.js
+++ test/hello_spec.js
@@ -1,7 +1,11 @@
 var sayHello = require('../src/hello');
 
+console.log('check');
+
 describe("Hello", function() {
+
   it("says hello", function() {
     expect(sayHello('world')).toBe("Hello, world!");
   });
+
 });
