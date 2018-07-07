c34f43c9d64a4b7a5e77609a0729a78c13f22a2b
Dover <jarilvalenciano@gmail.com>
2018-07-06 22:35:57 -0400

    added check for dirty values, initial watch value and accept watchers that omit the listener function

--- src/scope.js
+++ src/scope.js
@@ -5,21 +5,33 @@ var _ = require('lodash');
 
 function Scope() {
   this.$$watchers = [];
-};
+}
+
+function initWatchVal() {};
 
 Scope.prototype.$watch = function(watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
-    listenerFn: listenerFn
+    listenerFn: listenerFn || function() {},
+    last: initWatchVal
   };
 
-  this.$$watchers.push(watcher)
+  this.$$watchers.push(watcher);
 };
 
 Scope.prototype.$digest = function() {
+  var self = this;
+  var newValue, oldValue;
   _.forEach(this.$$watchers, function(watcher) {
-    watcher.listenerFn();
-  })
-}
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue,
+        (oldValue === initWatchVal ? newValue : oldValue),
+        self);
+    }
+  });
+};
 
-module.exports = Scope
+module.exports = Scope;
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -2,22 +2,92 @@
 var Scope = require('../src/scope');
 
 describe("Scope", function() {
+
   it("can be constructed and used as an object", function() {
       var scope = new Scope();
       scope.aProperty = 1;
       expect(scope.aProperty).toBe(1);
   });
+
   describe("digest", function() {
     var scope;
     beforeEach(function() {
       scope = new Scope();
     });
+
     it("calls the listener function of a watch on first $digest", function() {
       var watchFn = function() { return 'wat'; };
       var listenerFn = jasmine.createSpy();
       scope.$watch(watchFn, listenerFn);
       scope.$digest();
       expect(listenerFn).toHaveBeenCalled();
+    });
+
+    it("calls the watch function with the scope as the argument", function() {
+      var watchFn = jasmine.createSpy();
+      var listenerFn = function() {};
+      scope.$watch(watchFn, listenerFn);
+
+      scope.$digest();
+
+      expect(watchFn).toHaveBeenCalledWith(scope);
+    });
+
+    it("calls the listener function when the watched value changes", function() {
+      scope.someValue = 'a';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) { return scope.someValue; },
+        function(newValue, oldValue, scope) { scope.counter++; }
+      );
+
+      expect(scope.counter).toBe(0);
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.someValue = 'b';
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+    });
+
+    it("calls listener when watch value is first undefined", function() {
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) { return scope.someValue; },
+        function(newValue, oldValue, scope) { scope.counter++; }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
+
+    // newValue and oldValue will become 123 once digest is called
+    // point is that the initValue never gets leaked out of scope.js
+    // if initvalue is overwritten, oldvalue is set as the newvalue
+    it("calls listener with new value as old value the first time", function() {
+      scope.someValue = 123;
+      var oldValueGiven;
+
+      scope.$watch(
+        function(scope) { return scope.someValue; },
+        function(newValue, oldValue, scope) { oldValueGiven = oldValue; }
+      );
+
+      scope.$digest();
+      expect(oldValueGiven).toBe(123);
+    });
+
+    it("may have watchers that omit the listener function", function() {
+      var watchFn = jasmine.createSpy().and.returnValue('something');
+      scope.$watch(watchFn);
+
+      scope.$digest;
+      expect(watchFn).toHaveBeenCalled;
     });
   });
 });
