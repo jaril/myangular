d989fcd029bd249d4315179d14a1da10237e5769
Dover <jarilvalenciano@gmail.com>
2018-07-11 12:59:00 -0400

    added $destroy

--- src/scope.js
+++ src/scope.js
@@ -260,6 +260,7 @@ Scope.prototype.$new = function(isolated, parent) {
   parent.$$children.push(child);
   child.$$watchers = [];
   child.$$children = [];
+  child.$parent = parent;
   return child;
 };
 
@@ -275,6 +276,17 @@ Scope.prototype.$$everyScope = function(fn) {
   } else {
     return false;
   }
+};
+
+Scope.prototype.$destroy = function() {
+  if (this.$parent) {
+    var siblings = this.$parent.$$children;
+    var indexOfThis = siblings.indexOf(this);
+    if (indexOfThis >= 0) {
+      siblings.splice(indexOfThis, 1);
+    }
+  }
+  this.$$watchers = null;
 };
 
 module.exports = Scope;
