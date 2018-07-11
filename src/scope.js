af59d46e47a5747511c1ef70361fdd66c67dc6a8
Dover <jarilvalenciano@gmail.com>
2018-07-11 12:40:40 -0400

    implemented isolated scopes
    - isolated scopes do not have access to parent'(s) attributes
    - isolated scopes cannot watch its parent'(s) attributes
    - parent scopes digest their isolated children
    - isolated scopes digest from $$root on $apply, $evalAsync, $postDigest and $applyAsync

--- src/scope.js
+++ src/scope.js
@@ -82,8 +82,8 @@ Scope.prototype.$digest = function() {
   this.$beginPhase("$digest"); // starting digest phase
 
   //if there's a timeout set and digest gets called, flush the timeout
-  if (this.$$applyAsyncId) {
-    clearTimeout(this.$$applyAsyncId);
+  if (this.$root.$$applyAsyncId) {
+    clearTimeout(this.$root.$$applyAsyncId);
     this.$$flushApplyAsync();
   }
 
@@ -169,8 +169,8 @@ Scope.prototype.$applyAsync = function(expr) {
   self.$$applyAsyncQueue.push(function() {
     self.$eval(expr);
   });
-  if (self.$$applyAsyncId === null) {
-    self.$$applyAsyncId = setTimeout(function() {
+  if (self.$root.$$applyAsyncId === null) {
+    self.$root.$$applyAsyncId = setTimeout(function() {
         self.$apply(_.bind(self.$$flushApplyAsync, self));
     }, 0);
   }
@@ -184,7 +184,7 @@ Scope.prototype.$$flushApplyAsync = function() {
       console.error(e);
     }
   }
-  this.$$applyAsyncId = null;
+  this.$root.$$applyAsyncId = null;
 };
 
 Scope.prototype.$$postDigest = function(fn) {
@@ -243,10 +243,19 @@ Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
   };
 };
 
-Scope.prototype.$new = function() {
-  var ChildScope = function() { };
-  ChildScope.prototype = this;
-  var child = new ChildScope();
+Scope.prototype.$new = function(isolated) {
+  var child;
+  if (isolated) {
+    child = new Scope();
+    child.$root = this.$root;
+    child.$$asyncQueue = this.$$asyncQueue;
+    child.$$postDigestQueue = this.$$postDigestQueue;
+    child.$$applyAsyncQueue = this.$$applyAsyncQueue;
+  } else {
+    var ChildScope = function() { };
+    ChildScope.prototype = this;
+    child = new ChildScope();
+  }
   this.$$children.push(child);
   child.$$watchers = [];
   child.$$children = [];
