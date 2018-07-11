ce40b827d81a7379f170f17c01447fc648a8a61a
Dover <jarilvalenciano@gmail.com>
2018-07-11 09:17:30 -0400

    added handling of attribute shadowing
    - new child properties shadows its parents properties
    - wrapping a parent property in an object allows the child to mutate it

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -911,5 +911,28 @@ describe("Scope", function() {
       expect(aa.anotherValue).toBeUndefined();
       expect(aaa.anotherValue).toBeUndefined();
     });
+
+    it("shadows a parent's property with the same name", function() {
+      var parent = new Scope();
+      var child = parent.$new();
+
+      parent.name = 'Joe';
+      child.name = 'Jill';
+
+      expect(child.name).toBe('Jill');
+      expect(parent.name).toBe('Joe');
+    });
+
+    it("does not shadow members of parent scope's attributes", function() {
+
+      var parent = new Scope();
+      var child = parent.$new();
+
+      parent.user = {name: 'Joe'};
+      child.user.name = 'Jill';
+
+      expect(child.user.name).toBe('Jill');
+      expect(parent.user.name).toBe('Jill');
+    });
   });
 });
