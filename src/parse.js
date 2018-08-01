76a53ce891905ce8b303123d8ee4548578a88933
Dover <jarilvalenciano@gmail.com>
2018-08-01 10:02:34 -0400

    added string parsing

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var _ = require('lodash');
+
+var ESCAPES = {'n':'\n', 'f':'\f', 'r':'\r', 't':'\t',
+  'v':'\v', '\'':'\'', '"':'"'};
+
 function Lexer() {
 }
 
@@ -15,6 +20,8 @@ Lexer.prototype.lex = function(text) {
     if (this.isNumber(this.ch) ||
       (this.ch === '.' && this.isNumber(this.peek()))) {
       this.readNumber();
+    } else if (this.ch === '\'' || this.ch === '\"') {
+      this.readString(this.ch);
     } else {
       throw 'Unexpected next character: ' + this.ch;
     }
@@ -53,6 +60,47 @@ Lexer.prototype.readNumber = function() {
     text: number,
     value: Number(number)
   });
+};
+
+Lexer.prototype.readString = function(quote) {
+  this.index++;
+  var string = '';
+  var escape = false;
+
+  while (this.index < this.text.length) {
+    var ch = this.text.charAt(this.index);
+    if (escape) {
+      if (ch === 'u') {
+        var hex = this.text.substring(this.index + 1, this.index + 5);
+        if (!hex.match(/[\da-f]{4}/i)) {
+          throw 'Invalid unicode escape';
+        }
+        this.index += 4;
+        string += String.fromCharCode(parseInt(hex, 16));
+      } else {
+        var replacement = ESCAPES[ch];
+        if (replacement) {
+          string += replacement;
+        } else {
+          string += ch;
+        }
+      }
+      escape = false;
+    } else if (ch === quote) {
+      this.index++;
+      this.tokens.push({
+        text: string,
+        value: string
+      });
+      return;
+    } else if (ch === '\\') {
+      escape = true;
+    } else {
+      string += ch;
+    }
+    this.index++;
+  }
+  throw 'unmatched quote';
 };
 
 Lexer.prototype.peek = function() {
@@ -104,8 +152,22 @@ ASTCompiler.prototype.recurse = function(ast) {
       this.state.body.push('return ', this.recurse(ast.body), ';');
       break;
     case AST.Literal:
-      return ast.value;
+      return this.escape(ast.value);
   }
+};
+
+ASTCompiler.prototype.escape = function(value) {
+  if (_.isString(value)) {
+    return '\'' + value.replace(this.stringEscapeRegex, this.stringEscapeFn) + '\'';
+  } else {
+    return value;
+  }
+};
+
+ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
+
+ASTCompiler.prototype.stringEscapeFn = function(c) {
+  return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
 };
 
 function Parser(lexer) {
