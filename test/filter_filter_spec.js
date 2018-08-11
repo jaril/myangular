d01c06848e8cb5c4bfb6fe46f1228c3314f3bbf6
Dover <jarilvalenciano@gmail.com>
2018-08-11 16:44:23 -0400

    added filtering with predicate functions

--- test/filter_filter_spec.js
+++ test/filter_filter_spec.js
@@ -1,10 +1,26 @@
 'use strict';
 
+var parse = require('../src/parse');
 var filter = require('../src/filter').filter;
 
 describe('filter filter', function() {
 
   it('is available', function() {
     expect(filter('filter')).toBeDefined();
+  });
+
+  it('can filter an array with a predicate function', function() {
+    var fn = parse('[1, 2, 3, 4] | filter:isOdd');
+    var scope = {
+      isOdd: function(n) {
+        return n % 2 !== 0;
+      }
+    };
+    expect(fn(scope)).toEqual([1, 3]);
+  });
+
+  it('can filter an array of strings with a string', function() {
+    var fn = parse('arr | filter:"a"');
+    expect(fn({arr: ["a", "b", "a"]})).toEqual(['a', 'a']);
   });
 });
