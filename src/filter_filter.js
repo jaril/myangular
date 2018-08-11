721b1342f60f658aa2b883d2e61c1ae70bc5e949
Dover <jarilvalenciano@gmail.com>
2018-08-11 16:59:52 -0400

    added filtering with strings

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -4,8 +4,39 @@ var _ = require('lodash');
 
 function filterFilter() {
   return function(array, filterExpr) {
-    return _.filter(array, filterExpr)
+    var predicateFn;
+    if (_.isFunction(filterExpr)) {
+      predicateFn = filterExpr;
+    } else if (_.isString(filterExpr)) {
+      predicateFn = createPredicateFn(filterExpr);
+    } else {
+      return array;
+    }
+
+    return _.filter(array, predicateFn);
   };
 }
 
 module.exports = filterFilter;
+
+function createPredicateFn(expression) {
+
+  function comparator(actual, expected) {
+    actual = actual.toLowerCase();
+    expected = expected.toLowerCase();
+    return actual.indexOf(expected) !== -1
+  }
+  return function predicateFn(item) {
+    return deepCompare(item, expression, comparator)
+  };
+}
+
+function deepCompare(actual, expected, comparator) {
+  if (_.isObject(actual)) {
+    return _.some(actual, function(value) {
+      return deepCompare(value, expected, comparator);
+    });
+  } else {
+    return comparator(actual, expected);
+  }
+}
