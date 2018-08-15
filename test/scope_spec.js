7bb1ceb703ccd9ca6be72788536fe7aca3dca1df
Dover <jarilvalenciano@gmail.com>
2018-08-15 13:41:01 -0400

    added stateful filters

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1,6 +1,7 @@
 'use strict';
 var _ = require('lodash'); //no directory since dependency
 var Scope = require('../src/scope');
+var register = require('../src/filter').register;
 
 describe("Scope", function() {
 
@@ -817,6 +818,27 @@ describe("Scope", function() {
       scope.$digest();
       expect(values.length).toBe(2);
       expect(values[1]).toEqual([1, 2, 4]);
+    });
+
+    it('allows $stateful filter value to change over time', function(done) {
+      register('withTime', function() {
+        return _.extend(function(v) {
+          return new Date().toISOString() + ': ' + v;
+        }, {
+          $stateful: true
+        });
+      });
+      var listenerSpy = jasmine.createSpy();
+      scope.$watch('42 | withTime', listenerSpy);
+      scope.$digest();
+      var firstValue = listenerSpy.calls.mostRecent().args[0];
+
+      setTimeout(function() {
+        scope.$digest();
+        var secondValue = listenerSpy.calls.mostRecent().args[0];
+        expect(secondValue).not.toEqual(firstValue);
+        done();
+      }, 100);
     });
   });
 
