6f821e318f197c6147c78e93a8746d1b182bfd6d
Dover <jarilvalenciano@gmail.com>
2018-08-19 23:22:01 -0400

    all constants are registered before providers

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -408,6 +408,19 @@ describe('injector', function() {
       injector.get('aProvider');
     }).toThrow();
   });
+
+  it('registers constants first to make them available to providers', function() {
+    var module = window.angular.module('myModule', []);
+
+    module.provider('a', function AProvider(b) {
+      this.$get = function() { return b; };
+    });
+
+    module.constant('b', 42);
+    var injector = createInjector(['myModule']);
+    
+    expect(injector.get('a')).toBe(42);
+  });
 });
 
 describe('annotate', function() {
