3e7ea3b1b414f60711e52dd609dd34670fc1d0a4
Dover <jarilvalenciano@gmail.com>
2018-08-26 22:14:31 -0400

    deferreds are now resolved
    Promise callbacks are then invoked with the deferred value

--- src/q.js
+++ src/q.js
@@ -1,24 +1,46 @@
 function $QProvider() {
 
-  this.$get = function() {
-
+  this.$get = ['$rootScope', function ($rootScope) {
     function Promise() {
+      this.$$state = {};
     }
+
+    Promise.prototype.then = function(onFulfilled) {
+      //onFulfilled is the callback to be called on resolve
+      this.$$state.pending = onFulfilled;
+    };
 
     function Deferred() {
       this.promise = new Promise();
     }
 
+    Deferred.prototype.resolve = function(value) {
+      //calls the pending promise with the value
+      this.promise.$$state.value = value;
+      scheduleProcessQueue(this.promise.$$state);
+    };
+
     function defer() {
       return new Deferred();
+    }
+
+    function scheduleProcessQueue(state) {
+      $rootScope.$evalAsync(function() {
+        processQueue(state);
+      });
+    }
+
+    function processQueue(state) {
+      state.pending(state.value);
     }
 
     return {
       defer: defer
     };
-
-  };
+  }];
 
 }
+
+
 
 module.exports = $QProvider;
