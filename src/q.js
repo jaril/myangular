eee5dd1e240a6d5abb7b57dac3c45f2f8d0e71dd
Dover <jarilvalenciano@gmail.com>
2018-08-29 21:18:33 -0400

    added $q.resolve and $q.when

--- src/q.js
+++ src/q.js
@@ -119,6 +119,12 @@ function $QProvider() {
       return d.promise;
     }
 
+    function when(value, callback, errback, progressback) {
+      var d = defer();
+      d.resolve(value);
+      return d.promise.then(callback, errback, progressback);
+    }
+
     function scheduleProcessQueue(state) {
       $rootScope.$evalAsync(function() {
         processQueue(state);
@@ -150,7 +156,9 @@ function $QProvider() {
 
     return {
       defer: defer,
-      reject: reject
+      reject: reject,
+      when: when,
+      resolve: when
     };
   }];
 
