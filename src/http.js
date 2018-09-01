3c822e6e65346880f6c7cec6f49286e04cbe62da
Dover <jarilvalenciano@gmail.com>
2018-08-31 20:37:28 -0400

    added $http and $httpBackend

---
+++ src/http.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function $HttpProvider() {
+  this.$get = ['$httpBackend', function($httpBackend) {
+
+  }];
+}
+
+module.exports = $HttpProvider;
---
+++ src/http_backend.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function $HttpBackendProvider() {
+  this.$get = function() {
+    
+  };
+}
+
+module.exports = $HttpBackendProvider;
