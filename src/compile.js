d46c2e727a49c584924536258c05e9c3a73f4867
Dover <jarilvalenciano@gmail.com>
2018-09-13 21:19:42 -0400

    added directive prioritization

--- src/compile.js
+++ src/compile.js
@@ -21,9 +21,12 @@ function $CompileProvider($provide) {
         hasDirectives[name] = [];
         $provide.factory(name + 'Directive', ['$injector', function($injector) {
           var factories = hasDirectives[name];
-          return _.map(factories, function(factory) {
+          return _.map(factories, function(factory, i) {
             var directive = $injector.invoke(factory);
             directive.restrict = directive.restrict || 'EA';
+            directive.name = directive.name || name;
+            directive.index = i;
+            directive.priority = directive.priority || 0;
             return directive;
           });
         }]);
@@ -75,22 +78,36 @@ function $CompileProvider($provide) {
           addDirective(directives, directiveNormalize(match[1]), 'M');
         }
       }
+      directives.sort(byPriority);
       return directives;
+    }
+
+    function byPriority(a,b) {
+      var diff = b.priority - a.priority;
+      if (diff !== 0) {
+        return diff;
+      } else {
+        if (a.name !== b.name) {
+          return (a.name < b.name ? -1 : 1);
+        } else {
+          return a.index - b.index;
+        }
+      }
     }
 
     function nodeName(element) {
       return element.nodeName ? element.nodeName : element[0].nodeName;
     }
 
-  function addDirective(directives, name, mode) {
-    if (hasDirectives.hasOwnProperty(name)) {
-      var foundDirectives = $injector.get(name + 'Directive');
-      var applicableDirectives = _.filter(foundDirectives, function(dir) {
-        return dir.restrict.indexOf(mode) !== -1;
-      });
-      directives.push.apply(directives, applicableDirectives);
+    function addDirective(directives, name, mode) {
+      if (hasDirectives.hasOwnProperty(name)) {
+        var foundDirectives = $injector.get(name + 'Directive');
+        var applicableDirectives = _.filter(foundDirectives, function(dir) {
+          return dir.restrict.indexOf(mode) !== -1;
+        });
+        directives.push.apply(directives, applicableDirectives);
+      }
     }
-  }
 
     function applyDirectivesToNode(directives, compileNode) {
       var $compileNode = $(compileNode);
